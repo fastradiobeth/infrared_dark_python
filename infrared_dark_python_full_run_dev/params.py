72d00a2242d4af66a44e6b598755f39b1416dbb7
Beth Jones <beth@breadstick.home>
2015-08-27 16:08:09 +0100

    Setup and parameter files for continous version of infrared_dark_python source association catalogue and cutout production that does not run as independent scripts

---
+++ infrared_dark_python_full_run_dev/params.py
@@ -0,0 +1,94 @@
+# params.py
+# ******************************************************************************
+# parameters file required for run of infrared_dark_python
+# descriptions may be relocated to man page or other documentation
+# ******************************************************************************
+
+# survey setup
+# ------------------------------------------------------------------------------
+# survey codes:
+#   0 - Hi-GAL (use survey_params.py to add more surveys)
+survey_code = 0
+
+# survey wavelengths to use
+wavelengths = [160,250,350] # all wavelengths to be used
+wavelengths_required = [250,350]
+wavelengths_excluded = [160]
+reference_wavelength = 250
+
+# filesystem setup
+# ------------------------------------------------------------------------------
+# final '/' required for each location string
+cat_loc = '/home/bjones/Documents/IRDC_catalogues/' # location of input catalogues
+output_loc = '/home/bjones/Desktop/' # output location of cutouts
+cloud_loc = '/raid/scratch/bjones_IRDC/IRDCs/' # IRDC FITS files location
+
+# MOVE TO CUTOUT SCRIPT
+output_loc_cutouts = '/raid/scratch/bjones_IRDC/python_src_assoc_250_350_18.0asec_cutouts/' # TODO: need to make subdirectory in output_loc to store cutouts
+
+# single wavelength catalogue setup
+# ------------------------------------------------------------------------------
+
+# need to enter a single wavelength catalogue name for each value in 'wavelengths'
+single_wl_cats = {
+	70 : 'hyper_photometry_sig_var_FWHM_10_new_run_sources_only_70.dat',
+	160: 'hyper_photometry_sig_var_FWHM_10_new_run_sources_only_160.dat',
+	250: 'hyper_photometry_sig_var_FWHM_10_new_run_sources_only_250.dat',
+	350: 'hyper_photometry_sig_var_FWHM_10_new_run_sources_only_350.dat'
+}
+# single wavelength catalogue settings
+map_col = [0]
+coord_cols = [2,3]
+headerlines = 3
+# coord type
+#	0	-	Galactic longitude and latitude
+#	1	-	RA and DEC (J200)
+coord_type = 0
+
+
+# reference source setup
+# ------------------------------------------------------------------------------
+# choose settings for removal of duplicate entries/sources closer than a fixed
+# distance in reference wavelength source list
+
+# duplicate_filter_type
+#	0	-	only remove exact duplicates (default)
+#	1 	- 	select survey beam as separation of duplicates (filter_beam)
+#	2	-	set fixed separation (fixed_filter)
+#	3	- 	use same separation as for counterpart assignment
+duplicate_filter_type = 3
+filter_beam = 160 # duplicate_filter_type 1 only
+fixed_filter = 5 # arcseconds, duplicate_filter_type 2 only
+
+
+# counterpart association setup
+# ------------------------------------------------------------------------------
+
+# separation_type
+#	0	- miniumum survey beam of wavelengths_required (default)
+#	1	- select a survey beam_to_use as minimum separation
+#	2	-  set separation for source association to value of fixed_beam
+separation_type = 0
+fixed_beam = 5 # arcseconds, separation_type 1 only
+beam_to_use = 160 # survey beam, separation_type 2 only
+
+
+# cutout setup
+# ------------------------------------------------------------------------------
+
+# wavelengths of FITS files to cut from
+wavelengths_to_cut = [70,160,250,350,500]
+# TODO: specify naming of FITS files for import/ glob for them
+
+# codes for cutout width selection
+#	0 	-	use maximum angular diameter of starless sources
+#	2	- 	set to constant value in degrees, default_cut_width
+cut_width_type = 0
+default_cut_width = 0.02 # make sure this is the default setting
+
+# catalogue details of starless catalogue, cut_width_type = 0 only
+starless_cat_name = 'IRDC_l015_l055_temperature_mass_luminosity_run_160_350.dat'
+stl_headerlines = 3
+stl_map_col = [0]
+stl_rad_and_dist_cols = [4,8]	# source radius in pc and dist in kpc
+stl_coord_type = 0
---
+++ infrared_dark_python_full_run_dev/survey_params.py
@@ -0,0 +1,18 @@
+# survey_params.py
+# ******************************************************************************
+# because people should at least be given the option to use something that isn't
+# Hi-GAL
+# ******************************************************************************
+# beams in arcseconds assigned to wavelengths in microns
+
+higal_beams = {
+	70: 10.2,
+	160: 13.55,
+	250: 18.0,
+	350: 24.0,
+	500: 34.5
+}
+
+surveys = {
+    0: higal_beams
+}
