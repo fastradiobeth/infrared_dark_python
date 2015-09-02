3111a0a28254c37739635cddb925d59e68fa6af9
Beth Jones <beth@breadstick.home>
2015-09-02 14:13:41 +0100

    Addition of documentation to catalogue_compare module and upload of cutsources module which unintentionally was not uploaded previously

--- infrared_dark_python_full_run_dev/catalogue_compare.py
+++ infrared_dark_python_full_run_dev/catalogue_compare.py
@@ -1,7 +1,19 @@
 # catalogue_compare.py
 # tools module for comparing two source lists
+"""
+Award for Most Underdeveloped Module 2015: currently contains a function
+which compares two sets of coordinates (or anything else) to check for
+exact string matches. For use with infrared_dark_python_full_run.
+
+Development plan: add the features of catalogue_compare_alpha
+to catalogue_compare and enable dual mode functionality of this.
+"""
 
 def matched_sources(compare_A, compare_B):
+    """
+    Compares columns of arrays based on string characters and returns indices
+    of rows in A which also appear in B.
+    """
     # matches rows of A and B
     matched_inds = []
     for B_source in compare_B.T:
---
+++ infrared_dark_python_full_run_dev/cutsources.py
@@ -0,0 +1,69 @@
+# find FITS files available for cutting
+# --------------------------------------
+"""
+Tools for setting up required parameters for cutting large numbers of
+sources from FITS files and producing the cutouts. Designed for use
+with infrared_dark_python scripts.
+Required packages: datetime, re, glob, os, numpy, montage_wrapper
+Required infrared_dark_python files: coord_tools, params
+Note: Working Montage installation is required to use montage_wrapper
+"""
+import datetime
+import re
+import glob
+import os
+import coord_tools
+import numpy as np
+import montage_wrapper as montage
+
+def setup(catalogue_name):
+    """
+    Imports required parameters for cutting sources in catalogue
+    catalogue_name to specifications given in params.py. Finds all FITS
+    files available for cutting and selects width of cutouts to pass to
+    cut(). Also creates output subdirectoy to contain cutout files.
+    """
+    from params import cloud_loc, cut_width_type
+    # find all FITS files present- will include same cloud in multiple bands
+    if os.path.isdir(cloud_loc) == False:
+        sys.exit('IRDC FITS directory (cloud_loc) not found.')
+    # TODO: make this recursive search as different bands may be in subdirectories
+    filenames = glob.glob(cloud_loc + '*.fits')
+    # make subdirectory in output_loc for cutouts
+    cutout_dir = catalogue_name + '_' + str(datetime.date.today())
+    os.mkdir(cutout_dir)
+
+    # select cut width
+    # --------------------
+    if cut_width_type == 0:
+        # calculate and use maximum starless source width as cut width
+        from params import starless_cat_name, stl_headerlines, stl_rad_and_dist_cols, cat_loc
+        starless_cat = cat_loc + starless_cat_name
+        if os.path.isfile(starless_cat) == True:
+            rad_stl, dist_stl = np.loadtxt(starless_cat, skiprows=stl_headerlines, usecols=stl_rad_and_dist_cols, unpack=True)
+            print 'Starless catalogue imported.'
+        else:
+            sys.exit('Could not find starless catalogue. Quitting...')
+        width_stl = coord_tools.ang_diameter(rad_stl, dist_stl)
+        cut_width = max(width_stl)
+        print 'Cut width set to maximum width of catalogued starless objects:  ' + str(cut_width) + ' deg'
+    else:
+        # set cut width to constant angle in degrees
+        from params import default_cut_width
+        cut_width = default_cut_width
+
+    return cut_width, cutout_dir, filenames
+
+
+def cut(ra, dec, files, cut_width, input_directory, output_directory, catalogue_index=0):
+    """
+    Cuts a square region given by the input cut width centred on the
+    input coordinates (ra,dec) from all FITS file names provied.
+    FITS files (files) are expected to be in the input directory.
+    Catalogue index can be used to identify sources when cutouts
+    are produced from large catalogues, or defaults to 0 if not given.
+    """
+    for file in files:
+        # name assumes .fits files to remove suffix
+        output_name = output_directory + '/' + file[len(input_directory):-5] + '_' + str(catalogue_index) + '.fits'
+        montage.commands.mSubimage(file, output_name, ra, dec, cut_width)
