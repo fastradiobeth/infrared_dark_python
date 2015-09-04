5dcf4cd49db558db63e1485655651d0e66b3ff13
Beth Jones <beth@breadstick.home>
2015-09-04 12:59:01 +0100

    Addition of several functions to catalogue_compare module Further reduction in sarcasm level.

--- infrared_dark_python_full_run_dev/catalogue_compare.py
+++ infrared_dark_python_full_run_dev/catalogue_compare.py
@@ -1,13 +1,13 @@
 # catalogue_compare.py
 # tools module for comparing two source lists
 """
-Award for Most Underdeveloped Module 2015: currently contains a function
-which compares two sets of coordinates (or anything else) to check for
-exact string matches. For use with infrared_dark_python_full_run.
-
-Development plan: add the features of catalogue_compare_alpha
-to catalogue_compare and enable dual mode functionality of this.
+Tools module for running comparisons between source catalogues
+and lists, and writing outputs to files.
+Requires: numpy, os, sys
+Development plan: enable dual mode functionality
 """
+
+import numpy, os, sys
 
 def matched_sources(compare_A, compare_B):
     """
@@ -24,3 +24,71 @@ def matched_sources(compare_A, compare_B):
     matched_inds = list(set(matched_inds))
 
     return matched_inds
+
+def import_sources(catalogue_path, coord_cols, headerlines=0):
+    """
+    Imports arrays of glon/ra and glat/dec from a catalogue.
+    Requires numpy, os
+    """
+    if os.path.isfile(catalogue_path) == True:
+        x, y = np.loadtxt(catalogue_path, skiprows=headerlines, usecols=coord_cols, unpack=True)
+    else:
+        sys.exit('Could not find catalogue.')
+    return x, y
+
+def find_A_in_B(x_A, y_A, x_B, y_B):
+    """
+    Returns indicies of sources in catalogue A (x_A, y_A) that have a
+    match in catalogue B (x_B, y_B).
+    """
+    coords_A = np.array([x_A, y_A])
+    coords_B = np.array([x_B, y_B])
+    not_found_indices = []
+    found_indices = []
+    for index, source_A in enumerate(coords_A.T):
+        instances_matched = 0
+        for source_B in coords_B.T:
+            if source_B.tolist() == source_A.tolist():
+                instances_matched += 1
+        if instances_matched > 0:
+            # source was matched in B
+            found_indices.append(index)
+        return found_indices
+
+def find_A_not_in_B(x_A, y_A, x_B, y_B):
+    """
+    Returns indicies of sources in catalogue A (x_A, y_A) that do not
+    have a match in catalogue B (x_B, y_B).
+    """
+    coords_A = np.array([x_A, y_A])
+    coords_B = np.array([x_B, y_B])
+    not_found_indices = []
+    found_indices = []
+    for index, source_A in enumerate(coords_A.T):
+        instances_matched = 0
+        for source_B in coords_B.T:
+            if source_B.tolist() == source_A.tolist():
+                instances_matched += 1
+        if instances_matched == 0:
+            not_found_indices.append(index)
+        return not_found_indices
+
+def write_matches(ouput_path, indices, original_path, original_headerlines):
+    """
+    Writes new catalogue from original_catalogue to ouput_path in same format
+    containing only sources with line index in indices.
+    """
+    original_catalogue = open(original_path, 'r')
+    original_lines = original_catalogue.readlines()
+    original_header = original_lines[0:headerlines]
+    output_catalogue = open(output_path, 'w')
+    # adjust indices to account for headerlines in original catalogue
+    adjusted_indices = [x+headerlines for x in indices]
+    # find correct lines and write
+    output_lines = [line for index, line in enumerate(original_lines) if index in adjusted_indices]
+    output_catalogue.write(''.join(original_header))
+    output_catalogue.write(''.join(output_lines))
+    # close catalogues
+    output_catalogue.close()
+    original_catalogue.close()
+    return
