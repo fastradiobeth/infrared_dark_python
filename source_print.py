97308d0f4c8761120c9563e897d74e451491095e
Beth Jones <bethanymayjones@gmail.com>
2015-09-21 11:58:23 +0100

    Indentation issues fixed

--- source_print.py
+++ source_print.py
@@ -65,52 +65,52 @@ for x in range(total_sources):
   	for w in wavelengths:
   	  	cutout_names.append(cutout_loc + 'HGL' + str(cloud_coord) + '_' + str(w) + 'mu_J2000_'+str(x) +'.fits')
 
-    try:
-    	# figure that fits on one landscape A4
-     	fig_window = mpl.figure(figsize=(10,7))
-     	title = 'Cutouts of [250, 350]micron sources with no 160 counterpart from HGL'+ cloud_coord
-     	fig_window.suptitle(title, variant='small-caps')
-     	cloud_title = 'HGL' + cloud_coord
-     	# entire cloud
-     	f0 = aplpy.FITSFigure(cloud_name, figure=fig_window, subplot=[0.11,0.5,0.295,0.35])
-     	ax = fig_window.gca()
-     	ax.set_axis_bgcolor('black')
-     	large_label = cloud_title + '\n' + str(reference_wavelength) + '$\mu$m'
-     	f0.add_label(0.95,0.85, large_label, color='white', weight='bold', size='x-small', relative=True, horizontalalignment='right')
-     	f0.ticks.set_xspacing(0.04)
-     	f0.ticks.set_yspacing(0.02)
-     	f0.tick_labels.set_xformat('dd.dd')
-     	f0.tick_labels.set_yformat('dd.dd')
-     	f0.show_colorscale(stretch='log',cmap='gnuplot')
-     	f0.axis_labels.set_font(size='small')
-     	f0.tick_labels.set_font(size='small')
-     	f0.show_markers(ra[reference_wavelength][x], dec[reference_wavelength][x], edgecolor='blue', marker='*')
-
-    	# INDIVIDUAL WAVELENGTHS
-    	# --------------------------------------------------------------------------
-    	# set up standard subplot
-       	marker_colours = {
-            	70:'blue',
-            	160:'green',
-            	250:'red',
-            	350: 'black'
-        	}
+   	try:
+    		# figure that fits on one landscape A4
+     		fig_window = mpl.figure(figsize=(10,7))
+     		title = 'Cutouts of [250, 350]micron sources with no 160 counterpart from HGL'+ cloud_coord
+     		fig_window.suptitle(title, variant='small-caps')
+     		cloud_title = 'HGL' + cloud_coord
+     		# entire cloud
+     		f0 = aplpy.FITSFigure(cloud_name, figure=fig_window, subplot=[0.11,0.5,0.295,0.35])
+     		ax = fig_window.gca()
+     		ax.set_axis_bgcolor('black')
+     		large_label = cloud_title + '\n' + str(reference_wavelength) + '$\mu$m'
+     		f0.add_label(0.95,0.85, large_label, color='white', weight='bold', size='x-small', relative=True, horizontalalignment='right')
+     		f0.ticks.set_xspacing(0.04)
+     		f0.ticks.set_yspacing(0.02)
+     		f0.tick_labels.set_xformat('dd.dd')
+     		f0.tick_labels.set_yformat('dd.dd')
+     		f0.show_colorscale(stretch='log',cmap='gnuplot')
+     		f0.axis_labels.set_font(size='small')
+     		f0.tick_labels.set_font(size='small')
+     		f0.show_markers(ra[reference_wavelength][x], dec[reference_wavelength][x], edgecolor='blue', marker='*')
+	
+	    	# INDIVIDUAL WAVELENGTHS
+	    	# --------------------------------------------------------------------------
+	    	# set up standard subplot
+	       	marker_colours = {
+	            	70:'blue',
+       		     	160:'green',
+        	    	250:'red',
+        	    	350: 'black'
+        		}
 
         	def standard_setup(fig):
-            	s=15
-    		fig.set_system_latex(True)
-    		fig.axis_labels.hide()
-            	fig.tick_labels.set_xformat('dd.dd')
-            	fig.tick_labels.set_yformat('dd.dd')
-            	fig.ticks.set_xspacing=(0.02)
-            	fig.ticks.set_yspacing=(0.02)
-            	fig.tick_labels.set_font(size='small')
-            	fig.show_colorscale(stretch='log', cmap='gnuplot')
-    		ax = fig_window.gca()
-    		ax.set_axis_bgcolor('black')
-            	for w in coord_wavelengths:
-                		fig.show_markers(ra[w][x], dec[w][x], edgecolor=marker_colours[w], s=15)
-                		s +=2
+            		s=15
+    			fig.set_system_latex(True)
+    			fig.axis_labels.hide()
+        	    	fig.tick_labels.set_xformat('dd.dd')
+        	    	fig.tick_labels.set_yformat('dd.dd')
+        	    	fig.ticks.set_xspacing=(0.02)
+        	    	fig.ticks.set_yspacing=(0.02)
+        	    	fig.tick_labels.set_font(size='small')
+        	    	fig.show_colorscale(stretch='log', cmap='gnuplot')
+    			ax = fig_window.gca()
+    			ax.set_axis_bgcolor('black')
+        	    	for w in coord_wavelengths:
+        	        	fig.show_markers(ra[w][x], dec[w][x], edgecolor=marker_colours[w], s=15)
+        	        	s +=2
 
         	# 70 microns
         	f1 = aplpy.FITSFigure(cutout_names[0], figure=fig_window, subplot=[0.5,0.55,0.195,0.275])
@@ -136,10 +136,10 @@ for x in range(total_sources):
         	standard_setup(f5)
         	f5.tick_labels.hide()
         	f5.add_label(0.95,0.9,'500 $\mu$m', color='white', size='x-small', relative=True, variant='small-caps', horizontalalignment='right')
-
-        	# save figure
-        	output_name = output_loc + 'HGL' + cloud_coord + '_160_quiet_printing_' + str(x) +'.pdf'
-        	fig_window.savefig(output_name)
-        	f0.close()
-        except IOError:
-            print 'Problem finding FITS files for ' + cloud_title + ':  skipping source.... \n'
+	
+	        # save figure
+	        output_name = output_loc + 'HGL' + cloud_coord + '_160_quiet_printing_' + str(x) +'.pdf'
+	        fig_window.savefig(output_name)
+	        f0.close()
+	except IOError:
+		print 'Problem finding FITS files for ' + cloud_title + ':  skipping source.... \n'
